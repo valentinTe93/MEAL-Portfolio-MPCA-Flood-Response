240884b00d217bc6bade813e507ea6220066ffb1
valentinTe93 <seloneTe@outlook.com>
2026-09-15 23:03:50 +0000

    Connect MUI executive design system

--- 10_Dashboard/src/main.jsx
+++ 10_Dashboard/src/main.jsx
@@ -1,1 +1,14 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from './App';createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import App from './App';
+import { theme } from './theme/theme';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
