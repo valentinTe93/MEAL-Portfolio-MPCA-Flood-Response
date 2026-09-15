9dfcd593b104c8d46318015f5c92c5995737039a
valentinTe93 <seloneTe@outlook.com>
2026-09-15 17:57:07 +0000

    Configure GitHub Pages deployment

--- 10_Dashboard/vite.config.js
+++ 10_Dashboard/vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({base:'./',plugins:[react()]});
+
+export default defineConfig({
+  base: '/MEAL-Portfolio-MPCA-Flood-Response/',
+  plugins: [react()],
+});
